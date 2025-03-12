6a7f99487b76aa34c751de0ba490198ec3da6d8e
jkavdev <jhonatankolen@gmail.com>
2025-03-12 01:42:59 -0300

    Full Cycle 3.0 - Microsserviço: Assinaturas com Java ( Back-end ) - 1502 - tdd e implementacao do metodo subscriptionofid

--- infrastructure/src/main/java/br/com/jkavdev/fullcycle/subscription/infrastructure/gateway/repository/SubscriptionJdbcRepository.java
+++ infrastructure/src/main/java/br/com/jkavdev/fullcycle/subscription/infrastructure/gateway/repository/SubscriptionJdbcRepository.java
@@ -1,12 +1,16 @@
 package br.com.jkavdev.fullcycle.subscription.infrastructure.gateway.repository;
 
 import br.com.jkavdev.fullcycle.subscription.domain.account.AccountId;
+import br.com.jkavdev.fullcycle.subscription.domain.plan.PlanId;
 import br.com.jkavdev.fullcycle.subscription.domain.subscription.Subscription;
 import br.com.jkavdev.fullcycle.subscription.domain.subscription.SubscriptionGateway;
 import br.com.jkavdev.fullcycle.subscription.domain.subscription.SubscriptionId;
 import br.com.jkavdev.fullcycle.subscription.infrastructure.jdbc.DatabaseClient;
+import br.com.jkavdev.fullcycle.subscription.infrastructure.jdbc.RowMap;
 import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -32,8 +36,27 @@ public class SubscriptionJdbcRepository implements SubscriptionGateway {
     }
 
     @Override
-    public Optional<Subscription> subscriptionOfId(SubscriptionId anId) {
-        return Optional.empty();
+    public Optional<Subscription> subscriptionOfId(final SubscriptionId anId) {
+        final var sql = """
+                SELECT
+                    id,
+                    version,
+                    account_id,
+                    plan_id,
+                    created_at,
+                    updated_at,
+                    due_date,
+                    status,
+                    last_renew_dt,
+                    last_transaction_id
+                FROM subscriptions
+                WHERE id = :id
+                """;
+        return database.queryOne(
+                sql,
+                Map.of("id", anId.value()),
+                subscriptionMapper()
+        );
     }
 
     @Override
@@ -45,4 +68,20 @@ public class SubscriptionJdbcRepository implements SubscriptionGateway {
     public Optional<Subscription> latestSubscriptionOfAccount(AccountId accountId) {
         return Optional.empty();
     }
+
+    private RowMap<Subscription> subscriptionMapper() {
+        return (rs) -> Subscription.with(
+                new SubscriptionId(rs.getString("id")),
+                rs.getInt("version"),
+                new AccountId(rs.getString("account_id")),
+                new PlanId(rs.getLong("plan_id")),
+                rs.getDate("due_date").toLocalDate(),
+                rs.getString("status"),
+                rs.getObject("last_renew_dt", Instant.class),
+                rs.getString("last_transaction_id"),
+                rs.getObject("created_at", Instant.class),
+                rs.getObject("updated_at", Instant.class)
+        );
+    }
+
 }
--- infrastructure/src/test/java/br/com/jkavdev/fullcycle/subscription/infrastructure/gateway/repository/SubscriptionJdbcRepositoryTest.java
+++ infrastructure/src/test/java/br/com/jkavdev/fullcycle/subscription/infrastructure/gateway/repository/SubscriptionJdbcRepositoryTest.java
@@ -1,8 +1,16 @@
 package br.com.jkavdev.fullcycle.subscription.infrastructure.gateway.repository;
 
 import br.com.jkavdev.fullcycle.subscription.AbstractRepositoryTest;
+import br.com.jkavdev.fullcycle.subscription.domain.account.AccountId;
+import br.com.jkavdev.fullcycle.subscription.domain.plan.PlanId;
+import br.com.jkavdev.fullcycle.subscription.domain.subscription.SubscriptionId;
+import br.com.jkavdev.fullcycle.subscription.domain.subscription.status.ActiveSubscriptionStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.Instant;
+import java.time.LocalDate;
 
 class SubscriptionJdbcRepositoryTest extends AbstractRepositoryTest {
 
@@ -11,5 +19,37 @@ class SubscriptionJdbcRepositoryTest extends AbstractRepositoryTest {
         Assertions.assertNotNull(subscriptionRepository());
     }
 
+    @Test
+    @Sql({"classpath:/sql/subscriptions/seed-subscription-jhou.sql"})
+    public void givenPersistedSubscription_whenQueriesSuccessfully_shouldReturnIt() {
+        // given
+        Assertions.assertEquals(1, countSubscriptions());
+
+        final var expectedId = new SubscriptionId("5783bdbcbb2347eb8883e969f14d350c");
+        final var expectedAccountId = new AccountId("033c7d9eb3cc4eb7840b942fa2194cab");
+        final var expectedVersion = 1;
+        final var expectedPlanId = new PlanId(1L);
+        final var expectedStatus = ActiveSubscriptionStatus.ACTIVE;
+        final var expectedCreatedAt = Instant.parse("2024-04-28T10:58:11.111Z");
+        final var expectedUpdatedAt = Instant.parse("2024-04-28T10:59:11.111Z");
+        final var expectedDueDate = LocalDate.parse("2024-05-27");
+        final var expectedLastRenewDate = Instant.parse("2024-04-27T10:59:11.111Z");
+        final var expectedTransactionId = "560f4e6a-79fa-473c-b7cb-a5b2bb4e6c8a";
+
+        // when
+        final var actualSubscription = subscriptionRepository().subscriptionOfId(expectedId).orElseThrow();
+
+        // then
+        Assertions.assertEquals(expectedId, actualSubscription.id());
+        Assertions.assertEquals(expectedVersion, actualSubscription.version());
+        Assertions.assertEquals(expectedAccountId, actualSubscription.accountId());
+        Assertions.assertEquals(expectedPlanId, actualSubscription.planId());
+        Assertions.assertEquals(expectedStatus, actualSubscription.status().value());
+        Assertions.assertEquals(expectedCreatedAt, actualSubscription.createdAt());
+        Assertions.assertEquals(expectedUpdatedAt, actualSubscription.updatedAt());
+        Assertions.assertEquals(expectedDueDate, actualSubscription.dueDate());
+        Assertions.assertEquals(expectedLastRenewDate, actualSubscription.lastRenewDate());
+        Assertions.assertEquals(expectedTransactionId, actualSubscription.lastTransactionId());
+    }
 
 }
